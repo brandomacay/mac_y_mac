9413f388db8ae55bfd019abe82d124840709f57f
Alejandro Maceda Leon <maceda.alejandro@gmail.com>
2018-01-27 13:43:14 -0600

    Cambios de Ale :)

--- app/src/main/java/macay/maceda/reloj/checador/StartActivity.java
+++ app/src/main/java/macay/maceda/reloj/checador/StartActivity.java
@@ -3,12 +3,14 @@ package macay.maceda.reloj.checador;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.media.Image;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity implements View.OnClickListener {
@@ -25,48 +27,145 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.admin:
-                AlertDialog.Builder mBuilder = new AlertDialog.Builder(StartActivity.this);
-                View mView = getLayoutInflater().inflate(R.layout.dialog_admin, null);
-                final EditText password = (EditText) mView.findViewById(R.id.pass);
-                final EditText repeatpassword = (EditText) mView.findViewById(R.id.repeatpass);
-                Button cancel = (Button) mView.findViewById(R.id.cancel);
-                Button login = (Button) mView.findViewById(R.id.login);
 
-                mBuilder.setView(mView);
-                final AlertDialog dialog = mBuilder.create();
-                dialog.show();
-                cancel.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                       dialog.dismiss();
-                    }
-                });
+                     boolean isPasswordSet;
 
-                login.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        if(!password.getText().toString().isEmpty() && !repeatpassword.getText().toString().isEmpty()) {
-                            if (password.getText().toString().equals(repeatpassword.getText().toString())) {
-                                startActivity(new Intent(StartActivity.this, AdminActivity.class));
-                                Toast.makeText(StartActivity.this,
-                                        "Bienvenido!",
-                                        Toast.LENGTH_SHORT).show();
-                                dialog.dismiss();
-                            }else{
-                                repeatpassword.setError("Las contraseñas no coinciden");
-                            }
-                        }else{
-                            Toast.makeText(StartActivity.this,
-                                    "Ingresa todos los campos",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+                     isPasswordSet = PreferenceManager.getDefaultSharedPreferences(StartActivity.this)
+                     .getBoolean("isPasswordSet", false);
+
+                     if (isPasswordSet) {
+                         login_password_dialog();
+                     }
+                     else {
+                         create_password_dialog();
+                     }
+
+
+
+
                 break;
             case R.id.user:
                 startActivity(new Intent(StartActivity.this,MainActivity.class));
                 overridePendingTransition(0,0);
                 break;
         }
+    }
+
+
+    private void login_password_dialog () {
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(StartActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_admin, null);
+
+        final EditText password = (EditText) mView.findViewById(R.id.pass);
+        EditText repeatpassword = (EditText) mView.findViewById(R.id.repeatpass);
+        repeatpassword.setVisibility(View.GONE);
+        TextView tv = (TextView) mView.findViewById(R.id.textView);
+      //  tv.setVisibility(View.GONE);
+        tv.setText("Administrador");
+        Button cancel = (Button) mView.findViewById(R.id.cancel);
+        Button login = (Button) mView.findViewById(R.id.login);
+
+        mBuilder.setView(mView);
+        //mBuilder.setTitle("Crear una contraseña");
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                if (password.getText().toString().isEmpty()) {
+
+
+                    password.setError("Escribe una contraseña valida");
+
+                } else {
+                    String realPassword = PreferenceManager.getDefaultSharedPreferences(StartActivity.this)
+                            .getString("password", "");
+
+                    if (password.getText().toString().trim().equals(realPassword)) {
+
+                        Toast.makeText(StartActivity.this,
+                                "Bienvenido",
+                                Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                        startActivity(new Intent(StartActivity.this, AdminActivity.class));
+
+
+                    }
+                    else {
+                        Toast.makeText(StartActivity.this,
+                                "Error: La contraseña no coincide",
+                                Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                }
+
+            }
+        });
+    }
+
+    private void create_password_dialog () {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(StartActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_admin, null);
+        final EditText password = (EditText) mView.findViewById(R.id.pass);
+        final EditText repeatpassword = (EditText) mView.findViewById(R.id.repeatpass);
+        TextView tv = (TextView) mView.findViewById(R.id.textView);
+        //  tv.setVisibility(View.GONE);
+        tv.setText("Crear una contraseña");
+        Button cancel = (Button) mView.findViewById(R.id.cancel);
+        Button register = (Button) mView.findViewById(R.id.login);
+
+        mBuilder.setView(mView);
+        //mBuilder.setTitle("Crear una contraseña");
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                if (!password.getText().toString().isEmpty() && !repeatpassword.getText().toString().isEmpty()) {
+                    if (password.getText().toString().equals(repeatpassword.getText().toString())) {
+
+                        PreferenceManager.getDefaultSharedPreferences(StartActivity.this)
+                        .edit()
+                        .putBoolean("isPasswordSet", true)
+                        .putString("password", repeatpassword.getText().toString().trim())
+                        .commit();
+                       // startActivity(new Intent(StartActivity.this, AdminActivity.class));
+                        Toast.makeText(StartActivity.this,
+                                "Contraseña almacenada",
+                                Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                        //login_password_dialog();
+
+                    } else {
+                        repeatpassword.setError("Las contraseñas no coinciden");
+                    }
+                } else {
+                    Toast.makeText(StartActivity.this,
+                            "Ingresa todos los campos",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
