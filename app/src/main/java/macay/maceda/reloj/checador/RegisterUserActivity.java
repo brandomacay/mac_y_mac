eb12fc32238549f5d91c5895f6080f33a0c20696
Brandon Macay <brandomacay@gmail.com>
2018-01-27 17:28:07 -0500

    brandonMacay

--- app/src/main/java/macay/maceda/reloj/checador/RegisterUserActivity.java
+++ app/src/main/java/macay/maceda/reloj/checador/RegisterUserActivity.java
@@ -1,18 +1,147 @@
 package macay.maceda.reloj.checador;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
 
 public class RegisterUserActivity extends AppCompatActivity {
 
+    ImageView photo;
+    private static TextView birthday,date_trato;
+    Bitmap bitmappost;
+    boolean imageIsSet = false;
+    private static boolean birthday_picked = false;
+    private static int mYear, mMonth, mDay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+        birthday = (TextView) findViewById(R.id.register_birthday_tv);
+        date_trato = (TextView) findViewById(R.id.fecha_de_contrato);
+        photo = (ImageView) findViewById(R.id.foto);
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .setCropMenuCropButtonTitle("Elegir")
+                        .setCropShape(CropImageView.CropShape.RECTANGLE)
+                        .setOutputCompressFormat(Bitmap.CompressFormat.JPEG)
+                        .setOutputCompressQuality(75)
+                        .start(RegisterUserActivity.this);
+            }
+        });
+        birthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment newfrag = new DateBirthday();
+                newfrag.show(getFragmentManager(), "datePicker");
+            }
+        });
+        date_trato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment newfrag = new DateStartWork();
+                newfrag.show(getFragmentManager(), "datePicker");
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                try {
+                    bitmappost = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
+                    photo.setImageBitmap(bitmappost);
+                    imageIsSet = true;
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    imageIsSet = false;
+                }
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                imageIsSet = false;
+            }
+        }
+
+    }
+    public static class DateBirthday extends DialogFragment implements
+            DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // set default date
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            mYear = year;
+            mMonth = month;
+            mDay = day;
+
+            birthday.setText(new StringBuilder()
+                    .append(mYear).append("-")
+                    .append(mMonth + 1).append("-")
+                    .append(mDay).append(" "));
+            birthday_picked = true;
+
+        }
+    }
+    public static class DateStartWork extends DialogFragment implements
+            DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // set default date
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            mYear = year;
+            mMonth = month;
+            mDay = day;
+
+            date_trato.setText(new StringBuilder()
+                    .append(mYear).append("-")
+                    .append(mMonth + 1).append("-")
+                    .append(mDay).append(" "));
+            birthday_picked = true;
+
+        }
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
