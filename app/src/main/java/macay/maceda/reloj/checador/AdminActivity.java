df636e6a989427d6f8070f09c3e77505118f4e61
Brandon Macay <brandomacay@gmail.com>
2018-01-27 12:25:00 -0500

    brandonMacay

--- app/src/main/java/macay/maceda/reloj/checador/AdminActivity.java
+++ app/src/main/java/macay/maceda/reloj/checador/AdminActivity.java
@@ -1,19 +1,22 @@
 package macay.maceda.reloj.checador;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class AdminActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -26,4 +29,22 @@ public class AdminActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.admin_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.setting) {
+            startActivity(new Intent(AdminActivity.this,SettingsActivity.class));
+            overridePendingTransition(0,0);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
