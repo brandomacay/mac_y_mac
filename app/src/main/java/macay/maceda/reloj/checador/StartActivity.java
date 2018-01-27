01b2eedb533461ef445a8588beb3d67b42639de2
Brandon Macay <brandomacay@gmail.com>
2018-01-27 01:24:56 -0500

    brandonMacay

--- app/src/main/java/macay/maceda/reloj/checador/StartActivity.java
+++ app/src/main/java/macay/maceda/reloj/checador/StartActivity.java
@@ -1,11 +1,15 @@
 package macay.maceda.reloj.checador;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -21,7 +25,39 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.admin:
-                startActivity(new Intent(StartActivity.this,AdminActivity.class));
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(StartActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog_admin, null);
+                final EditText password = (EditText) mView.findViewById(R.id.pass);
+                final EditText repeatpassword = (EditText) mView.findViewById(R.id.repeatpass);
+                Button cancel = (Button) mView.findViewById(R.id.cancel);
+                Button login = (Button) mView.findViewById(R.id.login);
+
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                       dialog.dismiss();
+                    }
+                });
+
+                login.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(!password.getText().toString().isEmpty() && !repeatpassword.getText().toString().isEmpty()){
+                            startActivity(new Intent(StartActivity.this,AdminActivity.class));
+                            Toast.makeText(StartActivity.this,
+                                    "Bienvenido!",
+                                    Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                        }else{
+                            Toast.makeText(StartActivity.this,
+                                    "Ingresa todos los campos",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
                 break;
             case R.id.user:
                 startActivity(new Intent(StartActivity.this,MainActivity.class));
