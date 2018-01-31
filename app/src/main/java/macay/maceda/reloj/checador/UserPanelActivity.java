7c22ab34405f41b6578f27a5062bfe4230325e7c
Brandon Macay <brandomacay@gmail.com>
2018-01-31 18:28:52 -0500

    brandonMacay

--- app/src/main/java/macay/maceda/reloj/checador/UserPanelActivity.java
+++ app/src/main/java/macay/maceda/reloj/checador/UserPanelActivity.java
@@ -1,10 +1,16 @@
 package macay.maceda.reloj.checador;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.database.Cursor;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -150,9 +156,8 @@ public class UserPanelActivity extends AppCompatActivity {
                 //      "userid=" + String.valueOf(receivedPersonId) + " date=" +datex(),
                 //    Toast.LENGTH_SHORT).show();
 
-               dbHelper.insert_user_workout(String.valueOf(receivedPersonId), mWorkin, datetimex(),
-                       UserPanelActivity.this );
-               finish();
+
+                options_exit();
             }
         });
     }
@@ -163,6 +168,38 @@ public class UserPanelActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    private void options_exit () {
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(UserPanelActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_option_exit, null);
+
+        ImageView person_culminate = (ImageView) mView.findViewById(R.id.culminate);
+        ImageView person_food = (ImageView) mView.findViewById(R.id.food);
+
+        mBuilder.setView(mView);
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
+        person_culminate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbHelper.insert_user_workout(String.valueOf(receivedPersonId), mWorkin, datetimex(),
+                    UserPanelActivity.this );
+                Toast.makeText(UserPanelActivity.this,"Cuminacion de trabajo exitosa!",Toast.LENGTH_LONG).show();
+                dialog.dismiss();
+                finish();
+            }
+        });
+
+        person_food.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(UserPanelActivity.this,"Hora de comida",Toast.LENGTH_LONG).show();
+                dialog.dismiss();
+                finish();
+            }
+        });
     }
 
 
